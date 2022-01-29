fed5a945e13be31e2b0bdf70b19d03762527a722
yahuhen <yahuhen@gmail.com>
2022-01-30 02:45:05 +0300

    chrome capability

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -9,9 +11,17 @@ public class DemoTest {
 
     @Test
     public void sampleTest() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-dev-shm-usage");
+
         System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium/chrome");
         Configuration.browser = "chrome";
         Configuration.headless = true;
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
         open("https://demoqa.com/");
         $x("//h5[text()='Elements']").click();
     }
