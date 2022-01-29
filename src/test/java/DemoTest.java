1f69925303635dd8bdf3318c32148d2a0275a760
yahuhen <yahuhen@gmail.com>
2022-01-30 02:39:19 +0300

    chromium path

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -9,7 +9,8 @@ public class DemoTest {
 
     @Test
     public void sampleTest() {
-
+        System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium/chrome");
+        Configuration.browser = "chrome";
         Configuration.headless = true;
         open("https://demoqa.com/");
         $x("//h5[text()='Elements']").click();
